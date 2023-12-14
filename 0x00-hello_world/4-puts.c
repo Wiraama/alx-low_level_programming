054edca480bffa51d602f9d3254e85a64d524d60
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 12:47:53 -0800

    001

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#!/bin/bash
-include <stdio.h>
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
-	
-		return(0)
+	puts("Programming is like building a multilingual puzzle\n");
+
+		Return (0);
+
 }
