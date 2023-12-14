6233822f0507bb02c27a6af5ef7868b5ac467b58
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 14:02:07 -0800

    001

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-       	const char message[] = "and that piece of art is useful\"
-		Dora Korpar, 2015-10-19\n";
+       	const char message[] = "and that piece of art is useful\";
+	const char message[] = "Dora Korpar, 2015-10-19\n";
 
     /* Using write function to print to standard error */
 	 write(STDERR_FILENO, message, sizeof(message) - 1);
