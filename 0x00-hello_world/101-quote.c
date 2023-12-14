fd122f38cf575e00d77b643d036bbc8804777929
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 13:43:36 -0800

    001

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	const char  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char massage[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, message, sizeof(massage) - 1);
 	return (1);
 }
