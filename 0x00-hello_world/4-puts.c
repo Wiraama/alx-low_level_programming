56421c59fb3c790a5232dd55394ef4bca1ec672a
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 12:52:56 -0800

    001

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
-		Return (0);
+	return (0);
 
 }
