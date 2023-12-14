ef0f18df43c6681315fa252f2db2d0069318caab
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 12:57:29 -0800

    001

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,9 @@
-/**
- * program to print: with proper grammar, but the outcome is a piece of art
- *
- *
- */
 #include <stdio.h>
 /**
  * main - entry point 
  * prints "with proper grammar, but the outcome is a piece of art,"
  *
- * return: always 0
+ * Return: Always 0.
  */
 int main(void)
 {
