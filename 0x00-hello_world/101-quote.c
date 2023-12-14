70d9600480af11da2bb12e06bd8a99edf49adbab
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 14:46:57 -0800

    001

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - Entry point
  *
  * Description: Prints a message to standard error
- * 
+ *
  * Return: 1 (Error)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char * = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, 59);
 	
 	return (1);
