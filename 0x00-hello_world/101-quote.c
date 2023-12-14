6dcae54559ac4a6201494c73e7d4cabae02d5bcd
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 11:42:00 -0800

    001

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+
+/** 
+* program to print 'and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+* not to use printf nor puts
+* print a new line
+* direct to standard error
+*/
+int main(void)
+{
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
+	return(1)
+
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/*program to print: with proper grammar, but the outcome is a piece of art */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+		return(0)
+
+}
