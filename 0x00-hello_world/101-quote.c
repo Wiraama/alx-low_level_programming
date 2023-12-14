2d111e6a0d89d2310d50268ef29f3fea9106a75f
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-14 14:56:40 -0800

    001

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - this is the entry point
  *
- * Description: Prints a message to standard error
+ * Declarations: to print the assigned words in starndard error
  *
- * Return: 1 (Error)
+ * return: always 1.
  */
 int main(void)
 {
-	char * = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
 	return (1);
 }
+
+
