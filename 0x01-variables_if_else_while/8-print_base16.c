5b7aba529157311a3b8ae7a90880f2db62bee3dc
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-16 22:21:18 -0800

    corections

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num <= 9; num++)
+	/* Loop to print numbers 0-9 in base 16 */
+	for (i = 0; i < 10; i++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+		putchar('0' + i);
 	}
 
+	/* Loop to print lowercase letters a-f in base 16 */
+	for (i = 0; i < 6; i++)
+	{
+		putchar('a' + i);
+	}
+
+	/* Print a new line */
 	putchar('\n');
 
 	return (0);
