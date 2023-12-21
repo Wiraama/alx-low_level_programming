e2fe7930a1b3b5b544896b21ae932ba850873afc
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-21 15:47:49 -0800

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,11 @@ void print_triangle(int size)
 
         for (i = 0; i < size; i++)
         {
-            // Print spaces before the '#' characters
             for (j = 0; j < size - i - 1; j++)
             {
                 _putchar(' ');
             }
 
-            // Print the '#' characters
             for (j = 0; j < i + 1; j++)
             {
                 _putchar('#');
@@ -30,17 +28,4 @@ void print_triangle(int size)
             _putchar('\n');
 	}
     }
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
 }
