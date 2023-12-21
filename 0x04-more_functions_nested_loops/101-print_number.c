75241541952a94451edbcc4bebb76ea970667d16
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-21 13:32:48 -0800

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,48 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - a simplified version of putchar
+ * @c: The character to be printed
+ */
+void _putchar(char c)
+{
+    putchar(c);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n; // Make n positive for easier manipulation
+    }
+
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);
+    }
+
+    _putchar(n % 10 + '0');
+}
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_number(98);
+    print_number(123);
     _putchar('\n');
-    print_number(402);
+
+    print_number(-456);
     _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
+
     print_number(0);
     _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
+
     return (0);
 }
