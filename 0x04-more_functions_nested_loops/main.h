9ee960b64804fb7ce11b78edd7d64af023321808
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-21 15:17:49 -0800

    mawira

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
+/**
+ * mawira
+ */
 #ifndef MAIN_H
-#define MAIN_h
-/*Function prototype*/
+#define MAIN_H
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,10 +14,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-int _buzz(int 1, 100);
 void print_triangle(int size);
 void print_number(int n);
-int print_prime_numbers (int n);
 
-/*To end main_h*/
 #endif
