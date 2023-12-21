500b7e429f7ae15eb50a3ea38d620344f1e63a89
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-20 16:01:32 -0800

    my work

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,16 @@
-In file included from main.c:1:
-main.h:1:15: error: extra tokens at end of #ifndef directive [-Werror]
-    1 | #ifndef main_h:
-      |               ^
-main.h:1: error: unterminated #ifndef
-    1 | #ifndef main_h:
-      | 
-main.c: In function ‘main’:
-main.c:10:2: error: implicit declaration of function ‘print_alphabet’ [-Werror=implicit-function-declaration]
-   10 |  print_alphabet();
-      |  ^~~~~~~~~~~~~~
-cc1: all warnings being treated as errors
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+int_putchar(char c);
+void print_alphabet (void);
+void print_alphabet_x10(void);
+int islower (int c);
+int isalpha (int c);
+int print_sign(int n);
+int abs (int);
+int print_last digit (int);
+void jack bauer (void);
+void times table (void);
+int add(int, int);
+void print_to_98 (int n);
+
+#endif
