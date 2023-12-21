cd0c5a9a7cf1e614bf46d06dbf00d32b2b5d3342
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-21 13:38:42 -0800

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,15 @@ int main(void)
 {
     int num = 0;
 
-    while (num++ < 100)
+    while(num++ < 100)
     {
-        if ((num % 3 == 0) && (num % 5 == 0))
+        if((num % 3 == 0) && (num % 5 == 0))
             printf("FizzBuzz ");
-        else if ((num % 3) == 0)
+        else if((num % 3) == 0)
             printf("Fizz ");
-        else if ((num % 5) == 0)
+        else if((num % 5) == 0)
         {
-            if (num != 100)
+            if(num != 100)
                 printf("Buzz ");
             else
                 printf("Buzz");
