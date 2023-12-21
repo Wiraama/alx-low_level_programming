a6a8011958ec7bd9abd87c644cdeda9e03f7e31e
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-20 16:09:08 -0800

    my work

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet (void);
 void print_alphabet_x10(void);
 int islower (int c);
 int isalpha (int c);
 int print_sign(int n);
 int abs (int);
-int print_last digit (int);
-void jack bauer (void);
-void times table (void);
+int print_last_digit (int);
+void jack_bauer (void);
+void times_table (void);
 int add(int, int);
 void print_to_98 (int n);
 
