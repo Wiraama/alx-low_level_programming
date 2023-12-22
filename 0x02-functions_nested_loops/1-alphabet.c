5a363c35407515a712fa0b730e0ff7d015f901e3
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-21 16:57:24 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,17 @@
 /**
  * main - entry point
  *
- * 
  * Description: prints the alphabet in lowercase, followed by a new line.
  *
  * Return: Always 0
  */
 void print_alphabet(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
-
-    putchar('\n');
+       	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			putchar(letter);
+		}
+	putchar('\n');
 }
