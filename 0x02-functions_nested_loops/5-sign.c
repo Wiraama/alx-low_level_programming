6774e0a5e336564e3026883e140470e065cab8b8
Wiraama <mawira01@students.uonbi.ac.ke>
2023-12-23 00:58:27 -0800

    functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,26 @@
 #include "main.h"
 
-
 /**
  * print_sign - Prints the sign of a number
- * Description: program to print sigh=n number
+ * @n: The number to check
  *
- * Return 1 or -1
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
 {
-	int n;
-
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
-	return (0);
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
