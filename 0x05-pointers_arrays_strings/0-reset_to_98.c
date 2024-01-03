f792c60d31b612d308c283de2b94b6948f54a990
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-02 17:29:48 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - Entry point to the function
- * reset_to_98 - reset value of an integer to 98
- *
- * @n: pointer
- * Return: Always nothing
- */
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
