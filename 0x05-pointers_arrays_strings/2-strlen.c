8eeb962844873bd6cc0142c6523bac2d9be8f4c1
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-02 17:47:13 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * fuction toreturn the length of a string
+ * @_strlen - fuction toreturn the length of a string
  * @s: string
  *
  * Return: nothing
@@ -11,6 +11,6 @@ int _strlen(char *s)
 {
 	int index;
 
-	for(index = 0; s[index] != '\0' ; index++);
+	for(index = 0; s[index] != '\0'; index++);
 	return (index);
 }
