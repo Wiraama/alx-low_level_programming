ef684c08b02f7ebc94051b8ba038cf780c4817e9
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-02 17:25:56 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point to the function
  *
- * @reset: takes pointer to an int as a parameter and updates the value to 98.
+ * @reset_to_98: takes pointer to an int as a parameter and updates the value to 98.
  * @n: pointer
  * Return: Always nothing
  */
