737cd6a2c63e6464c86e9529d19f4eff319896de
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-04 15:49:13 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,24 +4,27 @@
  * @s: string
  * Return: encoded string `s`
  */
+char *leet(char *str) {
+    if (str == NULL) {
+        return NULL;  /**Handle the case of a NULL pointer**/
+    }
 
-char *leet(char *s)
-{
-	int i;
+    char *ptr = str;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+    while (*ptr) {
+        if (*ptr == 'a' || *ptr == 'A') {
+            *ptr = '4';
+        } else if (*ptr == 'e' || *ptr == 'E') {
+            *ptr = '3';
+        } else if (*ptr == 'o' || *ptr == 'O') {
+            *ptr = '0';
+        } else if (*ptr == 't' || *ptr == 'T') {
+            *ptr = '7';
+        } else if (*ptr == 'l' || *ptr == 'L') {
+            *ptr = '1';
+        }
+        ptr++;
+    }
 
-	return (s);
+    return str;
 }
