e81a2f6744725f0184ae1dca614a8c84b1edce9f
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-04 15:51:16 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,30 +1,24 @@
 #include "main.h"
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - encode string to 1337
+ * @s: string to encode
+ * Return: pointer to encoded string
  */
-char *leet(char *str) {
-    if (str == NULL) {
-        return NULL;  /**Handle the case of a NULL pointer**/
-    }
+char *leet(char *s)
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
 
-    char *ptr = str;
-
-    while (*ptr) {
-        if (*ptr == 'a' || *ptr == 'A') {
-            *ptr = '4';
-        } else if (*ptr == 'e' || *ptr == 'E') {
-            *ptr = '3';
-        } else if (*ptr == 'o' || *ptr == 'O') {
-            *ptr = '0';
-        } else if (*ptr == 't' || *ptr == 'T') {
-            *ptr = '7';
-        } else if (*ptr == 'l' || *ptr == 'L') {
-            *ptr = '1';
-        }
-        ptr++;
-    }
-
-    return str;
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
 }
