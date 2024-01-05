3ad98248601bd9744a4768786ae8451237805790
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-04 18:07:01 -0800

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,16 @@
+include "main.h"
+#include <stdio.h>
+
+/**
+ * main - cheak the code
+ * 
+ * Return: Always 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    char buffer[] = "This is a string!\0And this is the rest of the buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
+}
