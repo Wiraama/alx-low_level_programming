5962fa4508b9f19db937cee71f789e004aaf9cd1
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-04 16:56:52 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,26 +4,15 @@
  * print_number - print an integer
  * @num: integer to print
  */
-void printInteger(int num) {
-    if (num < 0) {
+void print_number(int n) {
+    if (n < 0) {
         _putchar('-');
-        num = -num;
+        n = -n;
     }
 
-    if (num == 0) {
-        _putchar('0');
-        return;
+    if (n / 10 != 0) {
+        print_number(n / 10);
     }
 
-    char buffer[20];  /**Assuming a 32-bit integer, which requires at most 11 digits plus a sign and null terminator**/
-    int index = 0;
-
-    while (num > 0) {
-        buffer[index++] = num % 10 + '0';
-        num /= 10;
-    }
-
-    for (int i = index - 1; i >= 0; i--) {
-        _putchar(buffer[i]);
-    }
+    _putchar(n % 10 + '0');
 }
