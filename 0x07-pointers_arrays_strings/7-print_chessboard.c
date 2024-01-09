a3e1c57a941700b78ae62cae191ff4baa02f14b7
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-08 17:52:41 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
-* print_chessboard - print chessboard given set 2D array
-* @a: 2D array
-*/
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i;
@@ -16,4 +16,3 @@ void print_chessboard(char (*a)[8])
 		_putchar('\n');
 	}
 }
-
