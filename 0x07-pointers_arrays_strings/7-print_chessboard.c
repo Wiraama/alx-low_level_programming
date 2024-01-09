ee0cc470520013770f46e6faf96e321a5905d96c
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-08 17:46:27 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,17 +4,28 @@
 * print_chessboard - print chessboard given set 2D array
 * @a: 2D array
 */
-
-void print_chessboard(char (*a)[8])
-{
-
-int row;
-int column;
-
-for (row = 0; row < 8; row++)
-{
-for (column = 0; column < 8; column++)
-_putchar(a[row][column]);
-_putchar('\n');
+void print_chessboard(char (*a)[8]) {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            printf("%c ", a[i][j]);
+        }
+        printf("\n");
+    }
 }
+
+int main(void) {
+    char chessboard[8][8] = {
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+    };
+
+    print_chessboard(chessboard);
+
+    return 0;
 }
