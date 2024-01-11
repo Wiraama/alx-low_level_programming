e2343858fac697b3e211629d730bef721b21c5bb
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-10 18:28:04 -0800

    C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,10 +9,14 @@
 
 int _pow_recursion(int x, int y)
 {
-  if(y < 0)
-  {
-    return(-1);
-  }
-  /**x^y = x*x^y-1**/
-  return x*_pow_recursion(x,y-1);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	/**x^y = x*x^y-1**/
+	return x * _pow_recursion (x, y - 1);
 }
