74d4f5f3e99934087df9937cdbd9d32e39f6f471
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-10 18:34:25 -0800

    C - Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,6 +3,9 @@
 
 /**
  * main - check the code
+ * function that returns the value of x raised to the power of y.
+ * @x: value
+ * @y:power
  *
  * Return: Always 0.
  */
@@ -18,5 +21,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 	/**x^y = x*x^y-1**/
-	return x * _pow_recursion (x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
