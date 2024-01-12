239eddafe7e194285c239ca71f35a6d86a49394f
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-12 14:40:26 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,17 +11,9 @@ int main(int argc, char* argv[])
 {
 	int count;
 	
-	printf("Program was called with \"%s\".\n",argv[0]);
-	if(argc > 1)
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 0; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}
-	else
-	{
-		printf("no more arguments");
+		printf("argv[%d] of string %s", count, argv[count]);
 	}
 	return (0);
 }
