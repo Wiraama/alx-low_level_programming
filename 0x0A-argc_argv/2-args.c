3f780d8d013ce21eba555eea02236adfddd03cc0
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-12 15:52:09 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * program that prints all arguments it receives.
+ * 
+ * @argc: receives numbers
+ * @argv: receives string typede
+ * 
+ * Return: always 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int count;
+	for(count = 0; count < argc; count++)
+	{
+		printf("%d\n%s\n", count, argv[count]);
+	}
+	return (0);
+}
