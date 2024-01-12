abbfc8ece13051917bd3f48b30c7f626f2877b45
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-12 14:44:51 -0800

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ * Prints the number of arguments passed to the program
+ *
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    printf("%d\n", argc - 1);
+
+    return 0;
+}
