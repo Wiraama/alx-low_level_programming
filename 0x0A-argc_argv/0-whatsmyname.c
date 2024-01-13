aae8d449c96ac828f0f0624c2f07727f33756d28
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-12 17:34:41 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,20 +4,20 @@
 /**
  * main - Entry point to the Program
  * program that prints its name, followed by a new line
- * 
+ *
  * @argc: number argument
- * @argv[]: string with comman-line arguments
+ * @argv: string with comman-line arguments
  *
  * Return: Always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int count;
-	
+
 	for (count = 0; count < argc; count++)
 	{
-		printf("%s\n",argv[count]);
+		printf("%s\n", argv[count]);
 	}
 	return (0);
 }
