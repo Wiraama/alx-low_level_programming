acb3a5c664bedbbca5300a56e878487179d1b4c5
Wiraama <mawira01@students.uonbi.ac.ke>
2024-01-12 17:16:18 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,12 @@
  * Return: always 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int count;
+
 	for (count = 0; count < argc; count++)
+
 	{
 		printf("%s\n", argv[count]);
 	}
