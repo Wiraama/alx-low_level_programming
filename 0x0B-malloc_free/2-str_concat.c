f85bcd06901c44c139dd82780177666b453a8353
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-16 17:33:37 -0800

    2-str_concat.c:16: warning: Excess function parameter

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - main prototype
+ * unction that concatenates two strings.
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: nothing
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t len_1;
+	size_t len_2;
+
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	/**calculate the length before concating**/
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	str = (char *)malloc(len_s1 + len_s2 + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(str, s1);/**copy s1 to str beforen join**/
+	strcat(str, s2);/**joining**/
+
+	return (str);
+} /**never mind about english WIRIAMA**/
