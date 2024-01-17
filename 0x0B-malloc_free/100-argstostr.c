e555112538524d8d194aeec7e38b81948d2c8afc
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-17 12:36:40 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - main entry point
+ * futction to concate all yor argument in your program
+ * @ac: hold total length in integer form
+ * @av: holda the string
+ *
+ * Return: nothing
+ */
+char *argstostr(int ac, char **av)
+{
+	int x, i, totalen;
+	char *result;
+
+	totalen = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	/** Let's calculate length of string**/
+	for (i = 0; i < ac; i++)
+	{
+		totalen += strlen(av[i]) + 1;/**+1 reps '\n'**/
+	}
+
+	/** Allocate memory on our machine using  malloc**/
+	result = (char *)malloc(sizeof(char));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	/**concating arguments**/
+	result[0] = '\0';
+	for (x = 0; x < result; x++)
+	{
+		strcat(result, av[i]);
+		strcat(result, "\n");
+	}
+	return (result);
+
+
+}
