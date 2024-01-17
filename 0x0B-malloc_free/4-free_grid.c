166c242174213ab6b63cc8bbac3b618ff9b800e8
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-16 18:27:28 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - the main entry point
+ * fuction to free memory of the privious project
+ * @height: array from the project
+ * @grid: pointer to pointer
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i <= height; i++)
+	{
+		free(int **grid);
+	}
+}
