8c3322b94755575ffa89e338ff66764d58584f1f
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-17 18:39:32 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,8 +10,8 @@
  */
 char **strtow(char *str)
 {
-    int i, count = 0, word = 0;
-    char **result, *token, *copy;
+    int i, j, count = 0, word = 0;
+    char **result;
 
     /** Quit if the string is empty **/
     if (str == NULL || *str == '\0')
@@ -19,54 +19,55 @@ char **strtow(char *str)
         return NULL;
     }
 
-    copy = strdup(str);  // Make a copy of the input string
-    if (copy == NULL)
-    {
-        return NULL;  // Memory allocation failed
-    }
-
     /** Word counting **/
-    token = strtok(copy, " ");
-    while (token != NULL)
+    while (*str)
     {
-        count++;
-        token = strtok(NULL, " ");
+        if (*str == ' ' || *str == '\0')
+        {
+            if (word)
+            {
+                count++;
+                word = 0;
+            }
+        }
+        else
+        {
+            word = 1;
+        }
+        str++;
     }
 
     /** Memory allocation **/
-    result = (char **)malloc((count + 1) * sizeof(char *));
+    result = (char **)malloc(count + 1);
     if (result == NULL)
     {
-        free(copy);
         return NULL;
     }
 
+    str = strtok(str, " "); /** Tokenize the original string **/
+
     /** Copying the words **/
     i = 0;
-    token = strtok(str, " ");
-    while (token != NULL)
+    while (str != NULL)
     {
-        result[i] = strdup(token);
+        result[i] = strdup(str); /** Duplicate each word using strdup **/
         if (result[i] == NULL)
         {
             /** Memory allocation failed, cleanup and return NULL **/
-            for (int j = 0; j < i; j++)
+            for (j = 0; j < i; j++)
             {
                 free(result[j]);
             }
             free(result);
-            free(copy);
             return NULL;
         }
 
         i++;
-        token = strtok(NULL, " ");
+        str = strtok(NULL, " ");
     }
 
     /** Set the last element of the array to NULL **/
     result[i] = NULL;
-
-    free(copy);  // Free the copied string
 
     return result;
 }
