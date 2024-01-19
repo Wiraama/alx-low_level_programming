fee4f0f852d681bb95fec2471ed9ef93dca5f3a9
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-19 11:50:36 -0800

    simple_print_buffer(a, 98);

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,25 +9,38 @@
  *
  * Return: malloc pointer
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	char *ptr;
 
-	/**checking if its null**/
 	if (nmemb == 0 || size == 0)
-	{
-		return (0);
-	}
-
-	/**allocating memoryb with malloc**/
-	ptr = malloc(nmemb * size);
-
-	/**check if malloc return null**/
-	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
