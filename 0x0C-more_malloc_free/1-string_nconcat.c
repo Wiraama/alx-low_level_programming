1faaeb00ea0a6d6d653312e86a25d183a86a92dd
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-19 17:30:02 -0800

    0x0A-argc_argv

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,31 +11,37 @@
  *
  * Return: results to main
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char  *ptr;
-	char *result;
+	char *ptr;
+	size_t len_s1 = (s1 != NULL) ? strlen(s1) : 0;
+	size_t len_s2 = (s2 != NULL) ? strlen(s2) : 0;
 
-	/**allocating memory with s1 first bye of s2 and a null terminator**/
-	ptr = malloc(strlen(s1) + n + 1);
+	ptr = malloc(len_s1 + ((n >= len_s2) ? len_s2 : n) + 1);
 
 	if (ptr == NULL)
 	{
-		return (NULL);
+		return NULL;
 	}
-	strcpy(ptr, s1);
-	/**f n is greater or equal to the length of s2 use the entire string s2**/
 
-	if (n >= strlen(s2))
+	if (len_s1 > 0)
 	{
-		strcat(ptr, s2);
+		memcpy(ptr, s1, len_s1);
 	}
-	else/**Concatenate the first n bytes of s2**/
-	{
-		strncat(ptr, s2, n);
-	}
-	result = ptr;
 
-	return (result);
+	if (n >= len_s2)
+	{
+		if (len_s2 > 0)
+		{
+			memcpy(ptr + len_s1, s2, len_s2);
+		}
+	}
+	else
+	{
+		memcpy(ptr + len_s1, s2, n);
+	}
+
+	ptr[len_s1 + ((n >= len_s2) ? len_s2 : n)] = '\0';
+
+	return ptr;
 }
