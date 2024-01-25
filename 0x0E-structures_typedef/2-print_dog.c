8abe0cc575b9d264a39686696c17b6875ad7cc89
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-24 21:04:22 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,8 +7,12 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name is: %s\n", (d->name != NULL) ? d->name : "(nil)");
-		printf("Age is: %f\n", d->age);
-		printf("Owner of the dog is: %s\n", (d->owner !=NULL) ? d->owner : "(nil)");
+		printf("(nil)");
+		return;
 	}
+
+	printf("Name is: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age is: %f\n", d->age);
+	printf("Owner of the dog is: %s\n", (d->owner !=NULL) ? d->owner : "(nil)");
+	
 }
