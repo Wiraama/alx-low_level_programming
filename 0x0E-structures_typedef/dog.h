ec47fd29dfafa480e163bc8365b33c4439741a08
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-24 17:54:12 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MY_DOG_H
 #define MY_DOG_H
-
+/**
+ * struct dog - entry point
+ * Define a new type struct dog with the following elements
+ * @name: name of the dog
+ * @age: age of the fucking dog
+ * @owner: name of the person owning the fucking dog
+ *
+ * Return: main
+ */
 typedef struct dog
 {
 	char *name;
