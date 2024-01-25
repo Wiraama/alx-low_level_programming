1979a2625bf3848cb6895346bd18e49d26c06f41
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-25 04:25:08 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * new_dog - main entry point
+ * @name: new member
+ * @owner: new member
+ * @age: new age
+ *
+ * Return: to main fuction
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	/**to ensure names are not emty**/
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	/** Allocating memory**/
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	/**to check if memory is allocated**/
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	/**copying/duplicating of the two strings and also creating memory**/
+	new_dog->name = malloc(strlen(name) + 1); /**+ 1 for null character**/
+
+
+	strcpy(new_dog->name, name);
+
+	new_dog->owner = malloc(strlen(owner) + 1);
+
+	strcpy(new_dog->owner, owner);
+
+	/**checkin ig memory allocation is successful**/
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->age = age;
+
+	return (new_dog);
+}
