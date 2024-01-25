01b638f7661d842c5393562ba3c80578778238fb
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-25 06:24:54 -0800

    mawira

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -24,10 +24,5 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "(nil)";
 	}
-	/**
-	printf("Name: %s\n", d->name);
-	printf("Age is: %f\n", d->age);
-	printf("Owner is: %s\n", d->owner);
-	**/
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
