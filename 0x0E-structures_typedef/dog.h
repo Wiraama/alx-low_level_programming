80dd0508803fabad6cef3c40d3363f2185d0b219
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-24 22:27:41 -0800

    mawira

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MY_DOG_H
-#define MY_DOG_H
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - entry point
  * Define a new type struct dog with the following elements
@@ -15,10 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
