05dcd030884ae18b5f89609f143a33cdac56e4a0
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-24 21:29:59 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,8 +2,22 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * init_dog - main entry point
+ * @d: passes stuct to main
+ * @name: name of the dog
+ * @age: integer to show old of dog
+ * @owner: the person owning dog
+ *
+ * Return: to main
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
