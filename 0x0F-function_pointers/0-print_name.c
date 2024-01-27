e0e25df693740885eb0046bc6e6d913653d5e8f2
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-26 21:17:58 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - main entry point
+ * @name: pointer to name to the person
+ * @f: pointer to function
+ *
+ * Return: to main function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
