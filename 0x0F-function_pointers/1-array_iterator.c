138a870680cb251fc9e94d0668fc97b54d0457bb
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-26 22:47:29 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,18 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+ * array_iterator - main entry point
+ * @array: member
+ * @size: bytes to be preserved
+ * @action: pointer array
+ *
+ * Return: to main function
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
