305097957531b6e76855f87ed223110064b13c9c
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-28 03:52:41 -0800

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry bpoint
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	int index, bytes;
+
+	if (argc == 2)
+	{
+		bytes = atoi(argv[1]);
+
+		if (bytes >= 0)
+		{
+			char *main_ptr = (char *)main; /**getting address of main**/
+
+			for (index = 0; index < bytes; index++)
+
+			{
+				printf("%02hhx", *(main_ptr + index));
+				if (index < bytes - 1)
+				{
+					printf(" ");
+				}
+				else
+				{
+					printf("\n");
+				}
+			}
+
+		}
+		printf("Error");
+		exit(2);
+	}
+	printf("Error");
+	exit(1);
+}
+
