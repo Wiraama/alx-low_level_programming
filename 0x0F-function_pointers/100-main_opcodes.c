4c46e0c14bd3943edaa3a8ab66f6fc77ae398372
Wiriama <mawira01@students.uonbi.ac.ke>
2024-01-28 04:19:12 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,33 +13,26 @@ int main(int argc, char *argv[])
 {
 	int index, bytes;
 
-	if (argc == 2)
+
+	if (argc != 2)
 	{
-		bytes = atoi(argv[1]);
-
-		if (bytes >= 0)
-		{
-			char *main_ptr = (char *)main; /**getting address of main**/
-
-			for (index = 0; index < bytes; index++)
-
-			{
-				printf("%02hhx", *(main_ptr + index));
-				if (index < bytes - 1)
-				{
-					printf(" ");
-				}
-				else
-				{
-					printf("\n");
-				}
-			}
-
-		}
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
 		printf("Error\n");
 		exit(2);
 	}
-	printf("Error\n");
-	exit(1);
-}
 
+	for (index = 0; index < bytes; index++)
+	{
+		printf("%02hhx", *((char *)main + index));
+		if (index < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
