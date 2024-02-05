e4ac875e00197868fc9646f07deba520a8c3d72a
Wiraama <mawira01@students.uonbi.ac.ke>
2024-02-05 13:22:25 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - main entry point
+ * @head: pointer to pointer pointing first node
+ *
+ * Return: freed memory
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	/** to free each node **/
+	while(*head != NULL)
+	{
+		temp = *head; /**allocating head detaials to temp **/
+		*head = (*head)->next; /**pointing to next node **/
+		free (temp);
+		*head = NULL;
+	}
+}
