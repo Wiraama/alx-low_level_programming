955993694601b55ea822e135657617791a11ea75
Wiraama <mawira01@students.uonbi.ac.ke>
2024-02-06 08:53:03 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
 
@@ -27,7 +26,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		free(new_node);
 
 		return (new_node);
 	}
@@ -38,15 +36,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 
-	new_node->next = temp->next;
-	temp->next = new_node;
 
 	/** checks if idx is beynd **/
 	if (temp == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
+	new_node->next = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
