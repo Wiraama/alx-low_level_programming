db3dd7b417b972dd6d6e4418564df41b25a5b9df
Wiraama <mawira01@students.uonbi.ac.ke>
2024-02-06 08:27:03 -0800

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - main entry point
+ * @head: first node
+ * @idx: index
+ * @n: integer
+ *
+ * Return: new created node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *new_node;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+
+	if (*head == NULL || idx == 0)
+	{
+		*head = new_node;
+
+		return (new_node);
+	}
+
+	temp = *head;
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+	{
+		temp = temp->next;
+	}
+
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	/** checks if idx is beynd **/
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	return (new_node);
+}
