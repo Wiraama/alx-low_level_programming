eee49cf38cb1867cd53be3f6a1b34c35f29bd7cd
Wiraama <mawira01@students.uonbi.ac.ke>
2024-02-05 20:56:34 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	temp = malloc(sizeof(listint_t));
 	/** to free each node **/
 	while (*head != NULL)
 	{
-		temp = *head; /**allocating head detaials to temp **/
-
+		temp = *head; /** creting value of head to temp **/
 		*head = (*head)->next; /**pointing to next node **/
-
 		free(temp);
 	}
 	*head = NULL;
