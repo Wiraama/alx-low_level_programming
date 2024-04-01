a8db043934b1542e5d2760bb1e35a470ff0d0c38
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-01 15:08:20 -0700

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,14 +1,19 @@
 #include "lists.h"
+
 /**
- * sum_dlistint - sums all nodes in a linked list
- * @head: head of the list
- * Return: the sum of the nodes
+ * sum_dlistint - main entry point
+ * @head: first node
+ *
+ * Return: summed
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum;
+	int sum = 0;
 
-	while (head)
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
