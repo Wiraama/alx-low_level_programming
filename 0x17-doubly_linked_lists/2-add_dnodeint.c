b7d591297f7dd7c5d5bea02e72adb44facff9151
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-01 13:25:13 -0700

    2-add_dnodeint.c;

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,33 @@
 #include "lists.h"
-/**
- * add_dnodeint - Adds a node to the beginning of a doubly linked list
- * @head: head of the linked list
- * @n: value to add to the list
- * Return: pointer to the element added
- */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp;
+	dlistint_t *newnode;
 
-	tmp = *head;
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
+	/*allocating memory**/
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	/**cheking if allocation is successfull**/
+	if (newnode == NULL)
+	{
 		return (NULL);
-	tmp->n = n;
-	tmp->prev = NULL;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
+	}
+
+	/**initialnizing**/
+	newnode->n = n;
+	newnode->prev = NULL;
+
+	if (*head == NULL)
+	{
+		newnode->next = NULL;
+		*head = newnode;
+	}
+	else
+	{
+		newnode->next = *head;
+		(*head)->prev = newnode;
+		*head = newnode;
+	}
+
+	return (newnode);
 }
