45848e657c2d8023800fb1d80496bb4d02a4516e
Wiraama <mawira01@students.uonbi.ac.ke>
2024-04-01 17:52:27 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,46 @@
 #include "lists.h"
-/**
- * insert_dnodeint_at_index - inserts a node at an index
- * @h: double pointer to a linked list
- * @idx: where to add the node
- * @n: value of the node
- * Return: pointer to the new node
- */
+
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *new;
+	dlistint_t *newnode, *temp;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
 		return (NULL);
-	new->n = n;
-	tmp = *h;
-	if (!tmp)
-	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
+
+	newnode->n = n;
+
+	/** checking if index is zeroc**/
 	if (idx == 0)
 	{
-		new->prev = NULL;
-		new->next = tmp;
-		tmp->prev = new;
-		*h = new;
-		return (new);
+		newnode->next = *h;
+		newnode->prev = NULL;
+		if (*h != NULL)
+		{
+			(*h)->prev = newnode;
+		}
+		*h = newnode;
 	}
-	for ( ; idx > 1 && tmp->next; idx--)
+
+	temp = *h;
+	while (temp != NULL && i < idx -1)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+	{
+		free(newnode);
 		return (NULL);
-	new->prev = tmp;
-	new->next = tmp->next ? tmp->next : NULL;
-	if (tmp->next)
-		tmp->next->prev = new;
-	tmp->next = tmp->next && idx > 1 ? NULL : new;
-	return (new);
+	}
+
+	newnode->next = temp->next;
+	newnode->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = newnode;
+	temp->next = newnode;
+
+	return (newnode);
 }
